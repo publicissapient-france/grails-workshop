462eca7b000edd2be8970f618304b72fa30ba847
Alexis Kinsella <alexis.kinsella@gmail.com>
2011-12-05 18:25:08 +0100

    Add of method beginsWith to render search result as JSON

--- grails-app/controllers/cocktail/IngredientController.groovy
+++ grails-app/controllers/cocktail/IngredientController.groovy
@@ -1,11 +1,18 @@
 package cocktail
 
+import grails.converters.JSON
+
 class IngredientController {
 
 	static scaffold = true
 
 	def hello = {
 		render 'Hello'
-	}		
+	}
+
+	def beginsWith() {
+    	def allMatching = Ingredient.findByNameIlike("${params.term}%")
+    	render allMatching as JSON
+	}
 
 }
