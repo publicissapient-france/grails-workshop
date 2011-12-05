c1d7075e38651d6e85aa7ea2294daef3419e5b92
Alexis Kinsella <alexis.kinsella@gmail.com>
2011-12-05 15:58:52 +0100

    Add fields to Recipe domain class

--- grails-app/domain/cocktail/Recipe.groovy
+++ grails-app/domain/cocktail/Recipe.groovy
@@ -1,7 +1,16 @@
 package cocktail
 
 class Recipe {
+    String name
+    String description
+
+    Glass glass
+
+    static hasMany = [ingredients: Ingredient]
 
     static constraints = {
+    name blank: false
+        description blank: false, size: 1..255
+        glass nullable: false
     }
 }
