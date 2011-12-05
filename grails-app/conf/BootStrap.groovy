732b29e716f0bfbb5bf55cbde36fdc4523a0975d
Alexis Kinsella <alexis.kinsella@gmail.com>
2011-12-05 15:25:37 +0100

    Add of dev records in BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,26 @@
+import cocktail.Ingredient
+import cocktail.Glass
+
 class BootStrap {
 
     def init = { servletContext ->
+
+		environments {
+        	development {
+          		[ Tumbler: 'Pour les long drinks.', Pilsner: 'Verre à demi' ].each { key, val ->
+            		new Glass(name: key, description: val).save()
+          		}
+
+          		[ Rhum: 'Bacardi', 'Citron vert': 'Agrume' ].each { key, val ->
+            		new Ingredient(name: key, description: val).save()
+          		}
+       		}
+     	}
+
     }
+
     def destroy = {
+
     }
+
 }
